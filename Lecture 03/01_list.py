c2b49c86ae9f440f7bcb1fef926e0a8485f4b165
codingwithrahat <codingwithrahat@gmail.com>
2026-02-15 20:34:47 +0600

    list

---
+++ Lecture 03/01_list.py
@@ -0,0 +1,23 @@
+#list is like array
+
+marks = [1, 2, 3, 4, 5, 6]
+
+print(marks)
+print(type(marks)) #list
+
+print(marks[0])   #1
+
+print(len(marks)) #6
+
+#can stor emultiple type data
+
+l = [1, "r", 3.0, True]
+
+print(l)
+
+#list are multable, we can update list
+print(l[2])
+
+l[2] = "Rahat"
+
+print(l[2])
---
+++ Lecture 03/03_list_methods.py
@@ -0,0 +1,42 @@
+#methods means function
+#funtions are same for list and string
+#but methods are spechific , like list specific methods
+
+l = [2, 1, 3]
+
+#append
+l.append(4)
+l.append(5)    #as like push_back()
+
+print(l)
+
+#sort
+l.sort()       #ascending order , it return NONE , it directly sort the function
+
+print(l) 
+
+#sort in desending
+l.sort(reverse = True) #desending order
+
+print(l)
+
+#reverse
+l.reverse() 
+
+print(l)
+
+#insert
+l.insert(1, 10)   #as like l[1] = 10
+
+print(l[1])  #10
+
+
+#remove
+l.remove(10) #remove value 10, first occurrence of value
+print(l)
+
+#pop
+l.pop(0) #remove index 0, value 1
+print(l)
+
+
